0ba79aa6f03a6d5f26e73f520fd7fc839c754d0e
Eslim Daga <eslimdaga@gmail.com>
2025-01-02 22:44:33 -0500

    feat:⭐️: update LocationCardProps interface; add new properties for regional offices, geolocation, and service details

--- types/location.ts
+++ types/location.ts
@@ -1,21 +1,24 @@
-export interface LocationCardProps {
-  location?: {
-    id: string;
-    serviceCenter: string;
-    department: string;
-    province: string;
-    district: string;
-    streetName: string;
-    publicServiceHours: string;
-    status: string;
-    localTypeAbbreviation: string;
-    takesPhoto: string;
-    deliversElectronicDNI: string;
-    dniMajorProcedure: string;
-    dniDeliveries: string;
-    civilRecordsRegistration: string;
-    civilRecordsCertification: string;
-    ruipnCertification: string;
-    erep: string;
-  } | null;
+export interface Location {
+  id: string;
+  regionalOffices: string;
+  department: string;
+  province: string;
+  district: string;
+  serviceCenter: string;
+  sioCode: string;
+  localTypeAbbreviation: string;
+  status: string;
+  publicServiceHours: string;
+  takesPhoto: string;
+  deliversElectronicDNI: string;
+  dniMajorProcedure: string;
+  dniDeliveries: string;
+  civilRecordsRegistration: string;
+  civilRecordsCertification: string;
+  ruipnCertification: string;
+  erep: string;
+  streetName: string;
+  latitude: number;
+  longitude: number;
+  geocoded_at: string;
 }
